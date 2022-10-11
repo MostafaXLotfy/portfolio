639e6ed3eca84099f47037b138e5af6eba715393
MostafaXLotfy <mostafaXlotfy@gmail.com>
2022-10-11 08:17:33 +0200

    Added responsiveness to the header

--- resources/js/header_generator.js
+++ resources/js/header_generator.js
@@ -1,14 +1,27 @@
 
 function generate_header(home=true){
     const prefix = home ? '.' : '..'
-    const element = document.getElementsByTagName('header')[0]
-    element.innerHTML = `
-        <h1><a href="${prefix}/index.html">Mostafa Lotfy</a></h1>
-        <nav>
-            <ul>
-                <li><a href="${prefix}/html/projects.html">Projects</a></li>
-                <li><a href="${prefix}/html/skills.html">Skills</a></li>
-            </ul>
-        </nav>
-    `
-}
+    const body = document.getElementsByTagName('body')[0]
+    body.innerHTML = `
+        <header class="desktop-header">
+            <h1><a href="${prefix}/index.html">Mostafa Lotfy</a></h1>
+            <nav>
+                <ul>
+                    <li><a href="${prefix}/html/projects.html">Projects</a></li>
+                    <li><a href="${prefix}/html/skills.html">Skills</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <header class="mobile-header">
+            <nav>
+                <ul>
+                    <li><a href="${prefix}/index.html">Mostafa Lotfy</a></li>
+                    <li><a href="${prefix}/html/projects.html">Projects</a></li>
+                    <li><a href="${prefix}/html/skills.html">Skills</a></li>
+                </ul>
+            </nav>
+        </header>
+    ` + body.innerHTML
+
+} 
